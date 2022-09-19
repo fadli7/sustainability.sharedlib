e9fe833a346f7b0ec96699e0f61252535bc2fcd2
CCN\fadli.farham <fadli.farham@ccn.com.sg>
2022-09-19 14:38:28 +0800

    add export carbon emission airline service

---
+++ index.js
@@ -0,0 +1,3 @@
+const { aggregateCEA, bulkCreateCEA, createCEA, findAllCEA, findOneCEA, updateCEA } = require('./services/carbon-emissions-airline.service');
+
+module.exports = { aggregateCEA, bulkCreateCEA, createCEA, findAllCEA, findOneCEA, updateCEA };
