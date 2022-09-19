03074e113e0431ed7357d410da31f05fcd160ae9
CCN\fadli.farham <fadli.farham@ccn.com.sg>
2022-09-19 15:45:56 +0800

    add all method function

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const { aggregateCEA, bulkCreateCEA, createCEA, findAllCEA, findOneCEA, updateCEA } = require('./services/carbon-emissions-airline.service');
+const { bulkCreateCER, createCER, findAllCER, findOneCER, updateCER } = require('./services/carbon-emissions-route.service');
+const { bulkCreateCE, createCE, findAllCE, findOneCE, updateCE } = require('./services/carbon-emissions.service');
+const { createDocs, findAllDocs, findOneDocs } = require('./services/documents.service');
+const { createQueue, deleteQueue, findAllQueue, updateQueue } = require('./services/queue.service');
 
-module.exports = { aggregateCEA, bulkCreateCEA, createCEA, findAllCEA, findOneCEA, updateCEA };
+module.exports = {
+  aggregateCEA, bulkCreateCEA, createCEA, findAllCEA, findOneCEA, updateCEA,
+  bulkCreateCER, createCER, findAllCER, findOneCER, updateCER,
+  bulkCreateCE, createCE, findAllCE, findOneCE, updateCE,
+  createDocs, findAllDocs, findOneDocs,
+  createQueue, deleteQueue, findAllQueue, updateQueue,
+};
