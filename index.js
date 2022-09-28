4e0559c3a3f3e9f0aaea261d0aa2918a0d2138ba
CCN\fadli.farham <fadli.farham@ccn.com.sg>
2022-09-28 14:54:23 +0800

    add aggregate ce to index

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const { aggregateCEA, bulkCreateCEA, createCEA, findAllCEA, findOneCEA, updateCEA } = require('./services/carbon-emissions-airline.service');
 const { bulkCreateCER, createCER, findAllCER, findOneCER, updateCER, aggregateCER } = require('./services/carbon-emissions-route.service');
-const { bulkCreateCE, createCE, findAllCE, findOneCE, updateCE } = require('./services/carbon-emissions.service');
+const { bulkCreateCE, createCE, findAllCE, findOneCE, updateCE, aggregateCE } = require('./services/carbon-emissions.service');
 const { createDocs, findAllDocs, findOneDocs } = require('./services/documents.service');
 const { createQueue, deleteQueue, findAllQueue, updateQueue } = require('./services/queue.service');
 
 module.exports = {
   aggregateCEA, bulkCreateCEA, createCEA, findAllCEA, findOneCEA, updateCEA,
   bulkCreateCER, createCER, findAllCER, findOneCER, updateCER, aggregateCER,
-  bulkCreateCE, createCE, findAllCE, findOneCE, updateCE,
+  bulkCreateCE, createCE, findAllCE, findOneCE, updateCE, aggregateCE,
   createDocs, findAllDocs, findOneDocs,
   createQueue, deleteQueue, findAllQueue, updateQueue,
 };
